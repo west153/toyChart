7249d6b39bee3bfc72eed381a1ed69a09064fc7f
kimdaseul <west1585@gmail.com>
2020-01-10 18:17:17 +0900

    파이차트 추가

--- app/src/main/java/com/example/toy/android/utils/TimeToValue.kt
+++ app/src/main/java/com/example/toy/android/utils/TimeToValue.kt
@@ -1,5 +1,7 @@
 package com.example.toy.android.utils
 
+import com.example.toy.android.utils.EasingUtils.Companion.linear
+
 class TimeToValue(
   val startT: Float,
   val durationT: Float,
@@ -9,7 +11,7 @@ class TimeToValue(
   private val easingT = EasingUtils(startT, durationT + startT)
   private val easingV = EasingUtils(minV, maxV)
 
-  fun easing(t: Float, easing: (Float) -> Float) = when {
+  fun easing(t: Float, easing: ((Float) -> Float) = ::linear) = when {
     t in easingT.min..easingT.max -> easingV.value(easing.invoke(easingT.ratio(t)))
     t < startT -> minV
     else -> maxV
